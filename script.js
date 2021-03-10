0315701d1edb677405605153140a227a17e28494
Luke Paschka <lpaschka37@gmail.com>
2021-03-10 16:41:37 -0600

    saving .js file work this far

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 // Assignment Code
+
+
+var number ["0,1,2,3,4,5,6,7,8,9"];
+var letter ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+var special ["!,@,#,$,%,^,&,*,?"];
+
 var generateBtn = document.querySelector("#generate");
+
 function generatePassword(){
   var finalValue = "";
   var characterCount = prompt("How many characters?");
   var includeSpecialCharacters = confirm("Allow special characters?");
   var includeNumbers = confirm("Include numbers?");
   var includeCapitalleters = confirm("Include capital letter?");
+
+  
   return finalValue
 }
 // Write password to the #password input
