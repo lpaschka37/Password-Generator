0f972a2c21af638b5a54fe8fb36736c05138e45e
Luke Paschka <lpaschka37@gmail.com>
2021-03-10 14:31:38 -0600

    added additional PW questions

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+function generatePassword(){
+  var finalValue = "";
+  var characterCount = prompt("How many characters?");
+  var includeSpecialCharacters = confirm("Allow special characters?");
+  var includeNumbers = confirm("Include numbers?");
+  var includeCapitalleters = confirm("Include capital letter?");
+  return finalValue
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
